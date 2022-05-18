1439e4ecfc7cf5049943a69ee4546808fc64f580
joanascheer <scheer.joana@gmail.com>
2022-05-18 16:22:59 -0300

    Implementar task 3 (informar quantidade legumes outros)

--- src/listacompras/ListaCompras.kt
+++ src/listacompras/ListaCompras.kt
@@ -1,8 +1,5 @@
 package listacompras
 
-import menu.Menu
-import menu.Menu.Companion.listaAlimentos
-
 /*Aqui chamamos de lista de compras apenas para ter as funções específicas da lista de compras que o sistema exige,
 afinal é um sistema de lista de compras. Não confundir com o conceito de List do Kotlin*/
 class ListaCompras {
@@ -39,41 +36,8 @@ class ListaCompras {
             return nome
         }
 
-        fun informarQuantidadeLegumeOutros(alimento: String): Int {
-            var unidades = 0
-            println("Informe quantas unidades de $alimento deseja: ")
-            val input = readln()
-
-            if (input.isEmpty() || input.isBlank()) {
-                println("Não é permitido inserir valor vazio")
-                informarQuantidadeLegumeOutros(alimento)
-            } else {
-                try {
-                    unidades = input.toInt()
-                    if (unidades < 0) {
-                        println("Não é permitido números negativos, tente novamente")
-                        informarQuantidadeLegumeOutros(alimento)
-                    }
-
-                } catch (exception: NumberFormatException) {
-                    println("Não é permitido texto, somente número")
-                    informarQuantidadeLegumeOutros(alimento)
-                }
-            }
-            return unidades
-        }
-
-        fun exibirLista(){
-            if (listaAlimentos.isEmpty()) {
-                println("Não contém nenhum item na lista")
-                Menu.menu()
-            } else {
-                listaAlimentos.forEach { alimento, quantidade ->
-                    println("Nome: $alimento - quantidade: $quantidade ")
-                }
-                println("\nA quantidade de itens a ser comprada é : ${Menu.listaAlimentos.size}\n")
-                Menu.menu()
-            }
+        fun informarQuantidadeLegumeOutros(alimento: String) {
+            //TODO:implementar lógica (3/3)
         }
 
     }
