18e29144abf98aabd6ae478da1876436f0f9b0c5
oliveiranatalia <natalia.oliveira@zup.com.br>
2022-05-19 11:30:58 -0300

    Corrigir tipo input alimento

--- src/listacompras/ListaCompras.kt
+++ src/listacompras/ListaCompras.kt
@@ -1,7 +1,5 @@
 package listacompras
 
-/*Aqui chamamos de lista de compras apenas para ter as funções específicas da lista de compras que o sistema exige,
-afinal é um sistema de lista de compras. Não confundir com o conceito de List do Kotlin*/
 class ListaCompras {
     companion object {
         fun informarQuantidadeVerduraGrao(alimento: String):Double{
@@ -11,7 +9,6 @@ class ListaCompras {
             if (input.isEmpty() || input.isBlank()) {
                 throw UnsupportedOperationException("Não é permitido inserir valor vazio")
             }
-
             var gramas = 0.0
             try {
                 gramas = input.toDouble()
@@ -19,7 +16,6 @@ class ListaCompras {
                     println("Não é permitido números negativos, tente novamente")
                     informarQuantidadeVerduraGrao(alimento)
                 }
-
             } catch (exception: NumberFormatException) {
                 println("Não é permitido texto, somente número")
             }
@@ -35,7 +31,6 @@ class ListaCompras {
             }
             return nome
         }
-
         fun informarQuantidadeLegumeOutros(alimento: String):Int{
             println("Informe a quantidade de $alimento em unidades: ")
             val input = readln()
@@ -43,7 +38,6 @@ class ListaCompras {
             if (input.isEmpty() || input.isBlank()) {
                 throw UnsupportedOperationException("Não é permitido inserir valor vazio")
             }
-
             var unidades = 0
             try {
                 unidades = input.toInt()
